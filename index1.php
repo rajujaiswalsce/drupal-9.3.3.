c2cf69e6d93469f323142053e2e8986acb2783ee
rajujaiswalsce <rajujaiswal220@gmail.com>
2023-12-05 12:23:06 +0530

    DSA

--- index1.php
+++ index1.php
@@ -76,7 +76,132 @@ for($i = 0;$i<count($x);$i++){
     }
 }
     
+$arr = [[1,2,3],
+[4,5,6],
+[7,8,9]];
+$x = 0;
+$y = 0;
+
+for($i=0;$i<count($arr);$i++){
+for($j=0;$j<count($arr[$i]);$j++){
+if($i === $j){
+$x += $arr[$i][$j] . "<br>"; 
+}else if($i === 2-$j){
+$y += $arr[$i][$j] . "<br>"; 
+}
+}
+}
+ echo  $x;
+
+ ?>
+ <?php
+// 2. Write a program to print half pyramid structure
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+ $n = 5;
+ $y = "";
+ for($i = 1;$i<=$n;$i++){
+  
+     for($j = $i;$j<=$i;$j++){
+      $y = $y . $i;
+     }
+    //  echo $y . '</br>';
+ }
 
 
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+$n = 5;
+ 
+ for($i = 1;$i<=$n;$i++){
+  $z = "";
+     for($j = 0;$j<$i;$j++){
+      $z = $z . $i;
+     }
+    //  echo $z . '</br>';
+ }
+//  *
+//  **
+//  ***
+//  ****
+//  *****
+$n = 5;
+ 
+ for($i = 1;$i<=$n;$i++){
+  $str = "";
+     for($j = 0;$j<$i;$j++){
+      $str = $str . "*" ;
+     }
+    //  echo $str . '</br>';
+ }
+?>
+
+
+<?php
+// 4. Reverse a number
+// Input
+// Integer - 12345
+// Output
+// Integer - 54321
+
+$arr = 12345;
+$str = (string)$arr;
+for($i= strlen($arr) - 1;$i>=0;$i--){
+    //  echo $str[$i];
+}
+?>
+
+<?php
+// 5. Print first 10 fibonacci numbers
+  $n=10;
+  function fib($n){
+  if($n === 0){
+        return 0; 
+  }else if($n === 1){
+        return 1; 
+  }else{
+    return (fib($n-2)+fib($n-1));
+  }
+} 
+ for ($i = 0; $i < $n; $i++){
+  // echo fib($i) ." " ;
+   }
+
+?>
+<?php
+// 3. Write a program to print half pyramid structure
+//     *
+//    ***
+//   ****
+//  *****
+// ******
+
+$n = 5;
+$str = "";
+for($i=1;$i<=$n;$i++){
+  for($j = 0;$j<$n-$i;$j++){
+      $str = $str . "_";
+  }
+     for($k=0;$k<$i;$k++){
+      $str = $str . "*";
+     }
+  
+  $str = $str . "</br>";
+}
+echo $str;
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
 
 
